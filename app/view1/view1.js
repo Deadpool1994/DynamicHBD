7f9e5a2dacf36c6f8a66e3fc3d67a8bb44121c12
deadpool1994 <mhtrami@gmail.com>
2016-10-25 01:11:19 +0530

    added callback function to trigger page change on time-end

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,14 +9,21 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope','$http', '$location',function($scope,$http,$location) {
 
+  var happyBirthday =  function(){
+    console.log('getting the time');
+    window.location.href = "/#!/view2";
+
+  console.log('gone');
+ };
     $(document).ready(function(){
       console.log('logged in');
       $(".digits").countdown({
         image: "img/digits.png",
         format: "dd:hh:mm:ss",
-        endTime: new Date('11/03/16 00:00:00')
+        endTime: new Date('10/25/16 01:05:00'),
+        timerEnd: function(){happyBirthday();}
       });
     });
 
